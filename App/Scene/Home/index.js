274b084316a52b0aedacc79a681a808122619856
Tri Pham <pvtri96@gmail.com.com>
2017-09-03 18:16:48 +0700

    Test scrollable-tab-view

--- App/Scene/Home/index.js
+++ App/Scene/Home/index.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import ScrollableTabView, {
+  ScrollableTabBar,
+} from 'react-native-scrollable-tab-view';
 import styles from './styles';
 
 class HomeScene extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <ScrollableTabView
+          initialPage={2}
+          renderTabBar={() => <ScrollableTabBar />}
+        >
+          <Text tabLabel="Tab #1">My</Text>
+          <Text tabLabel="Tab #2">favorite</Text>
+          <Text tabLabel="Tab #3">project</Text>
+          <Text tabLabel="Tab #4">favorite</Text>
+          <Text tabLabel="Tab #5">project</Text>
+        </ScrollableTabView>
       </View>
     );
   }
