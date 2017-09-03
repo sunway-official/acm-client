1474661f2ed94bc06739b2e8a5be1298c894e00b
Ly Bao Khanh <baokhanh7m@gmail.com>
2017-09-03 18:17:01 +0700

    Add colors

--- App/Theme/Colors.js
+++ App/Theme/Colors.js
@@ -1,40 +1,54 @@
+// ? See more details
+// ? https://material.io/guidelines/style/color.html#color-color-palette
+
 const colors = {
-  turquoise: '#1abc9c',
-  greenSea: '#16a085',
-  emerald: '#2ecc71',
-  nephritis: '#27ae60',
-  peterRiver: '#3498db',
-  belizeHole: '#2980b9',
-  amethyst: '#9b59b6',
-  wisteria: '#8e44ad',
-  wetAsphalt: '#34495e',
-  midnightBlue: '#2c3e50',
-  sunFlower: '#f1c40f',
-  orange: '#f39c12',
-  carrot: '#e67e22',
-  pumpkin: '#d35400',
-  alizarin: '#e74c3c',
-  pomegranate: '#c0392b',
-  cloudes: '#ecf0f1',
-  silver: '#bdc3c7',
-  concrete: '#95a5a6',
-  asbestos: '#7f8c8d',
-  inverse: '#081c24',
+  // core colors
+  red: '#F44336',
+  pink: '#E91E63',
+  purple: '#9C27B0',
+  deepPurple: '#673AB7',
+  indigo: '#3F51B5',
+  blue: '#2196F3',
+  lightBlue: '#03A9F4',
+  cyan: '#00BCD4',
+  teal: '#009688',
+  green: '#4CAF50',
+  lightGreen: '#8BC34A',
+  lime: '#CDDC39',
+  yellow: '#FFEB3B',
+  amber: '#FFC107',
+  orange: '#FF9800',
+  deepOrange: '#FF5722',
+  brown: '#795548',
+  grey: '#9E9E9E',
+  blueGrey: '#607D8B',
+  black: '#000000',
+  white: '#FFFFFF',
+
+  // custom colors
+  lightDeepOrange: '#FFAB91', // 200
+  darkDeepOrange: '#D84315', // 800
+  lightGrey: '#EEEEEE', // 200
+  darkGrey: '#BCBCBC',
+  darkRed: '#C62828', // 800
 };
 
 const defaultColor = {
-  primary: colors.emerald,
-  primaryDark: colors.nephritis,
-  secondary: colors.cloudes,
-  secondaryDark: colors.silver,
-  success: colors.emerald,
-  successDark: colors.nephritis,
+  primary: colors.deepOrange,
+  primaryLight: colors.lightDeepOrange,
+  primaryDark: colors.darkDeepOrange,
+  secondary: colors.lightGrey,
+  secondaryLight: colors.white,
+  secondaryDark: colors.darkGrey,
+  success: colors.deepOrange,
+  successDark: colors.darkDeepOrange,
   warning: colors.sunFlower,
   warningDark: colors.orange,
   info: colors.peterRiver,
   infoDark: colors.belizeHole,
-  danger: colors.alizarin,
-  dangerDark: colors.pomegranate,
+  danger: colors.red,
+  dangerDark: colors.darkRed,
+  textColor: colors.white,
 };
 
 export default {
