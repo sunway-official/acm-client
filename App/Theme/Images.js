d1cf307271a3177c2f0566f42aa4d504d73fb941
Ly Bao Khanh <baokhanh7m@gmail.com>
2017-09-04 10:13:49 +0700

    Add Images

--- App/Theme/Images.js
+++ App/Theme/Images.js
@@ -1,5 +1,16 @@
 const images = {
-  // TODO
+  // TODO: image default for test
+  imgDefault50: require('../Images/default-50.png'),
+  imgDefault100: require('../Images/default-100.png'),
+  imgDefault150: require('../Images/default-150.png'),
+  imgDefault200: require('../Images/default-200.png'),
+  imgDefault250: require('../Images/default-250.png'),
+
+  event: require('../Images/Icons/ic_event_black_24dp.png'),
+  eventAvailable: require('../Images/Icons/ic_event_available_black_24dp.png'),
+  eventBusy: require('../Images/Icons/ic_event_busy_black_24dp.png'),
+  notification: require('../Images/Icons/ic_notifications_black_24dp.png'),
+  search: require('../Images/Icons/ic_search_black_24dp.png'),
 };
 
 export default images;
