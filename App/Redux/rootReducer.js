feba95eb6768644367fe4c7d4d6f551401b5e3a7
Mars <baokhanh7m@hotmail.com>
2017-09-05 19:21:10 -0700

    Add redux to Login UI

--- App/Redux/rootReducer.js
+++ App/Redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import DrawerReducer, { KEY as DRAWER_KEY } from './Drawer';
 import NavigationReducer, { KEY as NAVIGATION_KEY } from './Navigation';
+import LoginReducer, { KEY as LOGIN_KEY } from './Login';
 
 export default appolo => {
   let reducers = {
     [DRAWER_KEY]: DrawerReducer,
     [NAVIGATION_KEY]: NavigationReducer,
+    [LOGIN_KEY]: LoginReducer,
     form: formReducer,
   };
   if (appolo) {
--- App/Scene/Login/index.js
+++ App/Scene/Login/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { KEY, setLoggedIn } from '../../Redux/Login';
 import LoginForm from '../../Component/Login/LoginForm';
 
 class LoginScene extends Component {
+  static propTypes = {
+    login: PropTypes.object,
+    setLoggedIn: PropTypes.func,
+    navigation: PropTypes.object,
+  };
+
   submit = values => {
     console.log(values);
+    this.props.setLoggedIn();
+    console.log(this.props.navigation);
   };
 
   render() {
@@ -12,4 +24,14 @@ class LoginScene extends Component {
   }
 }
 
-export default LoginScene;
+const mapStateToProps = state => ({
+  login: state[KEY],
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setLoggedIn: bindActionCreators(setLoggedIn, dispatch),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScene);
