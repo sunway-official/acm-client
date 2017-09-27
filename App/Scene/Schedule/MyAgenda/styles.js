045c84d651f9d23966af9005e21164a916d4e8e6
Tri Pham <pvtri96@gmail.com>
2017-09-27 13:22:45 +0700

    Change My Agenda Scene background color to white

--- App/Scene/Schedule/MyAgenda/styles.js
+++ App/Scene/Schedule/MyAgenda/styles.js
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { Colors } from '~/Theme';
 
 const Schedule = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 1,
+    backgroundColor: Colors.white,
   },
 });
 
