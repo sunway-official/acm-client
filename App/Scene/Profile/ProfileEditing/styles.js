704916af089fc0d15be4b5fea29869be05631627
Dung Le <dungle1811@gmail.com>
2017-09-30 10:24:25 +0700

    [HOTFIX] Change edit user profile form styles

--- App/Scene/Profile/ProfileEditing/styles.js
+++ App/Scene/Profile/ProfileEditing/styles.js
@@ -1,5 +1,24 @@
 import { StyleSheet } from 'react-native';
+import { Colors, Metrics } from '../../../Theme';
 
-const ProfileEditing = StyleSheet.create({});
+const { baseMargin, doubleBaseMargin } = Metrics;
+const { white, primary } = Colors;
+
+const ProfileEditing = StyleSheet.create({
+  container: {
+    margin: baseMargin,
+    backgroundColor: white,
+  },
+  formContainer: { padding: baseMargin },
+  inputContainer: { marginBottom: doubleBaseMargin },
+  submitBtn: {
+    backgroundColor: primary,
+    padding: baseMargin,
+  },
+  submitTitle: {
+    textAlign: 'center',
+    color: white,
+  },
+});
 
 export default ProfileEditing;
