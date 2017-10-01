443ffdf793e3517864304729b96541a52d9d0196
Tri Pham <pvtri96@gmail.com>
2017-10-01 09:46:09 +0700

    Disable Drawer Animation on Remote Debugger

--- App/Redux/Drawer/index.js
+++ App/Redux/Drawer/index.js
@@ -1,5 +1,8 @@
 export const TOGGLE_DRAWER = 'TOGGLE_DRAWER';
 export const SET_DRAWER_STATE = 'SET_DRAWER_STATE';
+export const DISABLE_GESTURES = 'DISABLE_GESTURES';
+export const ENABLE_GESTURES = 'ENABLE_GESTURES';
+export const SET_DISABLE_GESTURES = 'SET_DISABLE_GESTURES';
 
 export const KEY = 'drawer';
 
@@ -12,8 +15,22 @@ export const setDrawerState = isOpen => ({
   isOpen,
 });
 
+export const setDisableGestures = isDisableGestures => ({
+  type: SET_DISABLE_GESTURES,
+  isDisableGestures,
+});
+
+export const disableGestures = () => ({
+  type: DISABLE_GESTURES,
+});
+
+export const enableGestures = () => ({
+  type: ENABLE_GESTURES,
+});
+
 const INITIAL_STATE = {
   isOpen: false,
+  disableGestures: false,
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -27,6 +44,21 @@ export default (state = INITIAL_STATE, action) => {
       return {
         ...state,
         isOpen: action.isOpen,
+      };
+    case DISABLE_GESTURES:
+      return {
+        ...state,
+        disableGestures: true,
+      };
+    case SET_DRAWER_STATE:
+      return {
+        ...state,
+        disableGestures: false,
+      };
+    case SET_DISABLE_GESTURES:
+      return {
+        ...state,
+        disableGestures: action.isDisableGestures,
       };
     default:
       return state;
