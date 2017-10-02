d2b354d18fb44797918613a87afca35fc51784ac
Tri Pham <pvtri96@gmail.com>
2017-10-03 00:40:21 +0700

    Remove drawer statusbar backdrop

--- App/Navigation/Drawer/Menu/styles.js
+++ App/Navigation/Drawer/Menu/styles.js
@@ -10,7 +10,7 @@ export default StyleSheet.create({
   statusBar: {
     // paddingTop: Constants.statusBarHeight,
     height: Metrics.statusBarHeight,
-    backgroundColor: 'rgba(0,0,0,0.7)',
+    backgroundColor: 'rgba(0,0,0,0.0)',
     marginHorizontal: statusBarMargin,
   },
   headerContainer: {
