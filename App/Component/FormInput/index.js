9a77e577e68605e25a4e1f3ab793731f9668fe84
Tri Pham <pvtri96@gmail.com>
2017-10-05 18:15:22 +0700

    Change Text importing in FormInput

--- App/Component/FormInput/index.js
+++ App/Component/FormInput/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Text, TextInput, View } from 'react-native';
-
+import { TextInput, View } from 'react-native';
+import { Text } from '~/Component';
 import styles from './styles';
 import { Colors } from '~/Theme';
 
