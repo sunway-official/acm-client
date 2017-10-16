3a7744c2f6ca4509685efb999a5dffebacb11019
Mars <baokhanh7m@hotmail.com>
2017-10-16 18:49:50 +0700

    Add mutation insertNews to StatusPosting

--- App/Scene/NewsFeed/StatusPosting/index.js
+++ App/Scene/NewsFeed/StatusPosting/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View, TouchableOpacity, TextInput } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { UserAvatar, TouchableView, Text } from '~/Component';
@@ -7,27 +7,57 @@ import { Colors } from '~/Theme';
 import { defaultUserAvatar } from '../fixture';
 import styles from './styles';
 
-const StatusPosting = () => {
-  return (
-    <View style={styles.container}>
-      <UserAvatar small avatar={defaultUserAvatar} />
-      <TouchableView
-        rippleColor={Colors.secondary}
-        style={styles.statusBoxView}
-      >
-        <TextInput
-          style={[styles.placeholderStyle]}
-          placeholder="What's on your mind?"
-          underlineColorAndroid="transparent"
-        />
-      </TouchableView>
-      <TouchableOpacity>
-        <Icon name="camera" type="material-community" />
-      </TouchableOpacity>
-    </View>
-  );
-};
+import { graphql, gql, compose } from 'react-apollo';
+import INSERT_NEWS_MUTATION from '~/Graphql/mutation/insertNews.graphql';
 
-StatusPosting.propTypes = {};
+class StatusPosting extends Component {
+  static propTypes = {
+    insertNews: PropTypes.func,
+  };
 
-export default StatusPosting;
+  post(contentNews) {
+    this.props.insertNews({
+      userId: 11, // bk user id
+      conferenceId: 1, // fake conferences
+      contentNews,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <UserAvatar small avatar={defaultUserAvatar} />
+        <TouchableView
+          rippleColor={Colors.secondary}
+          style={styles.statusBoxView}
+        >
+          <TextInput
+            style={[styles.placeholderStyle]}
+            placeholder="What's on your mind?"
+            underlineColorAndroid="transparent"
+          />
+        </TouchableView>
+        <TouchableOpacity>
+          <Icon name="camera" type="material-community" />
+        </TouchableOpacity>
+        <TouchableView
+          rippleColor={Colors.primary}
+          onPress={() => this.post('this is new status')}
+        >
+          <Text>Post</Text>
+        </TouchableView>
+      </View>
+    );
+  }
+}
+
+const StatusPostingMutation = graphql(gql(INSERT_NEWS_MUTATION), {
+  props: ({ mutate }) => ({
+    insertNews: ({ userId, conferenceId, contentNews }) =>
+      mutate({
+        variables: { userId, conferenceId, contentNews },
+      }),
+  }),
+});
+
+export default compose(StatusPostingMutation)(StatusPosting);
