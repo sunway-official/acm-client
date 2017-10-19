adc9bc5b9c1ebad76a87d0b638877dbc88c0e2da
Mars <baokhanh7m@hotmail.com>
2017-10-19 12:59:22 +0700

    Refactor NewsFeedPosts

--- App/Component/News/Posts/index.js
+++ App/Component/News/Posts/index.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 import { Icon } from 'react-native-elements';
 
 import styles from './styles';
-import { Colors } from '~/Theme';
+import { Colors, Images } from '~/Theme';
 import {
   UserAvatar,
   TouchableView,
@@ -13,7 +13,7 @@ import {
   AutoExpandingTextInput,
 } from '~/Component';
 
-import { defaultUserAvatar } from '~/Scene/NewsFeed/fixture';
+const defaultAvatar = Images.avatar['male08'];
 
 class Posts extends Component {
   constructor(props) {
@@ -30,6 +30,71 @@ class Posts extends Component {
     this.props.cancel();
   }
 
+  _renderHeader() {
+    return (
+      <View style={styles.header}>
+        <TouchableView onPress={this.props.cancel}>
+          <Text bold style={{ color: Colors.primary }}>
+            Cancel
+          </Text>
+        </TouchableView>
+        <Text bold medium>
+          Update Status
+        </Text>
+        <TouchableView
+          onPress={this.post}
+          disabled={this.state.text === '' ? true : false}
+        >
+          <Text
+            bold
+            style={
+              this.state.text === ''
+                ? { color: Colors.grey }
+                : { color: Colors.primary }
+            }
+          >
+            Post
+          </Text>
+        </TouchableView>
+      </View>
+    );
+  }
+
+  _renderContents() {
+    return (
+      <View style={styles.content}>
+        <View style={styles.contentUserInformation}>
+          <UserAvatar small avatar={defaultAvatar} />
+          <Text bold style={styles.contentUsername}>
+            Ly Bao Khanh
+          </Text>
+        </View>
+        <AutoExpandingTextInput
+          value={this.state.text}
+          placeholder={"What's on your mind?"}
+          onChangeText={text => this.setState({ text })}
+          enablesReturnKeyAutomatically={true}
+          returnKeyType="done"
+        />
+      </View>
+    );
+  }
+
+  _renderActions() {
+    return (
+      <View style={styles.action}>
+        <TouchableView rippleColor={Colors.primary} borderless={true}>
+          <Icon name="md-photos" type="ionicon" />
+          <Text>Photo</Text>
+        </TouchableView>
+        <TouchableView rippleColor={Colors.primary} borderless={true}>
+          <Icon name="camera" type="material-community" />
+          <Text>Camera</Text>
+        </TouchableView>
+      </View>
+    );
+  }
+
   render() {
     const isVisible = this.props.isVisible;
 
@@ -42,58 +107,9 @@ class Posts extends Component {
         isVisible={isVisible}
         style={styles.container}
       >
-        <View style={styles.header}>
-          <TouchableView onPress={this.props.cancel}>
-            <Text bold style={{ color: Colors.primary }}>
-              Cancel
-            </Text>
-          </TouchableView>
-          <Text bold medium>
-            Update Status
-          </Text>
-          <TouchableView
-            onPress={this.post}
-            disabled={this.state.text === '' ? true : false}
-          >
-            <Text
-              bold
-              style={
-                this.state.text === ''
-                  ? { color: Colors.grey }
-                  : { color: Colors.primary }
-              }
-            >
-              Post
-            </Text>
-          </TouchableView>
-        </View>
-
-        <View style={styles.content}>
-          <View style={styles.contentUserInformation}>
-            <UserAvatar small avatar={defaultUserAvatar} />
-            <Text bold style={styles.contentUsername}>
-              Ly Bao Khanh
-            </Text>
-          </View>
-          <AutoExpandingTextInput
-            value={this.state.text}
-            placeholder={"What's on your mind?"}
-            onChangeText={text => this.setState({ text })}
-            enablesReturnKeyAutomatically={true}
-            returnKeyType="done"
-          />
-        </View>
-
-        <View style={styles.action}>
-          <TouchableView rippleColor={Colors.primary} borderless={true}>
-            <Icon name="md-photos" type="ionicon" />
-            <Text>Photo</Text>
-          </TouchableView>
-          <TouchableView rippleColor={Colors.primary} borderless={true}>
-            <Icon name="camera" type="material-community" />
-            <Text>Camera</Text>
-          </TouchableView>
-        </View>
+        {this._renderHeader()}
+        {this._renderContents()}
+        {this._renderActions()}
       </Modal>
     );
   }
