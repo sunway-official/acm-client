b47e45fa06558c2e2033eef2d4de003e2988299c
Mars <baokhanh7m@hotmail.com>
2017-11-12 22:35:15 +0700

    [HOTFIX] Fix HTTP_URL_REGEX

--- App/Lib/constants.js
+++ App/Lib/constants.js
@@ -1,6 +1,6 @@
 export const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,})/;
 export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-export const HTTP_URL_REGEX = /(^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$)/;
+export const HTTP_URL_REGEX = /^(https?:)?\/\//i;
 
 export default {
   PASSWORD_REGEX,
